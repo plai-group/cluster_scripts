6783c6531f850793d129a41e57a35a66c167e110
William Harvey <williamharvey08@gmail.com>
2024-01-26 19:16:59 -0800

    minor

--- will/start_jobs.py
+++ will/start_jobs.py
@@ -4,23 +4,12 @@ from time import sleep
 import itertools as it
 
 
-
-hostname = run('hostname', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True).stdout.strip()
-print(hostname)
-job_script, queue = {
-    'submit-ml': ('job.sh', 'ubcml-rti'),
-    'borg': ('job.sh', 'plai'),
-}[hostname.split('.')[0]]
-print(f"Submitting to", (job_script, queue,))
-
-
 def submit(command, jobname, n_gpus, n_hours):
     
     # set some environment variables - these will be passed though and accessible to the script we submit
     # (job.sh) inside the submitted job
     # COMMAND is what we use to pass through the Python command that will be run inside the job
     os.environ['COMMAND'] = command
-    os.environ['GPUS'] = str(n_gpus)
 
     # compile an 'sbatch' command to use to start the job
     # there are some hard-coded values in here like the memory allocation (12GB per GPU) and the number of
@@ -30,7 +19,7 @@ def submit(command, jobname, n_gpus, n_hours):
     # SCRATCH environment variable to point to a directory for this to work. I do so with the line
     # 'export SCRATCH="/ubc/cs/research/plai-scratch/wsgh"' in ~/.bashrc
     output_dir = os.path.join(os.environ['SCRATCH'], 'slurm-outputs')
-    sbatch_command = f"sbatch --export=all --ntasks=1 -t {n_hours}:0:0 --mem={12000*max(1, n_gpus)} --cpus-per-task {5*max(1, n_gpus)} --gres=gpu:{n_gpus} -J {jobname} --output=\"{output_dir}/%j.out\" --error=\"{output_dir}/%j.err\" job.sh"
+    sbatch_command = f"sbatch -p plai --export=all --ntasks=1 -t {n_hours}:0:0 --mem={12000*max(1, n_gpus)} --cpus-per-task {5*max(1, n_gpus)} --gres=gpu:{n_gpus} -J {jobname} --output=\"{output_dir}/%j.out\" --error=\"{output_dir}/%j.err\" job.sh"
 
     # exectute sbatch_command and store the output in 'result'
     result = run(sbatch_command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
